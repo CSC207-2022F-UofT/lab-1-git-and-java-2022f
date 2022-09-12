026b6fc62e43822b85979a629fe6adbf9ac98cf4
CC-3636 <natalieaduarte@gmail.com>
2022-09-12 17:47:27 -0400

    Natalie's changes

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -33,10 +33,7 @@ public class IntroLab {
         return message_to_print + message;
     }
 
-    /* TODO: In the space below, write your our method.
-     *       The method should be named with your utorID and should call
-     *       printLabMessage with some parameters.
-     *
+    /*
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
@@ -49,20 +46,25 @@ public class IntroLab {
                 "Welcome to CSC207!");
     }
 
+   /** Natalie's method:
+    * The following is my method
+    */
+    public static String duarten1() {
+        return printLabMessage("Hello there", "Natalie", false,
+                "Peace");
+    }
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
 
-        /* TODO: Add a call to the method you wrote in this main method,
-         *        and print the results of it.
-         *
+        /*
          * As a reference, we've provided a sample call to exampleStudent
          * above. Afterwards: run this file to see the output!
          */
 
+        System.out.println(duarten1());
+
 
     }
-
-    // TODO: Go to test/java/IntroLabTest.java afterwards and complete the TODO.
 
 }
