209890f18bf59ebfefbfafc3dc3de76ab113561f
Qingyu Xie <qingyu.xie@mail.utoronto.ca>
2022-09-12 15:40:08 -0400

    Added [xieqing7] and [testXieqing7]to IntroLab and relevant test in IntroLabTest

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+    public void testXieqing7() {
+        String expected = "Hello! My name is Qingyu Xie! " +
+                "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.exampleStudent();
+        assertEquals(expected, actual);
+    }
 }
