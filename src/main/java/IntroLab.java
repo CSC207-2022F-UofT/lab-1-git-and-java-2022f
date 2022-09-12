7c11f7b658e144a363caa7e92bad86674c40caaf
SeongminChang <seongmin.chang@mail.utoronto.ca>
2022-09-12 18:28:00 -0400

    Added changs70 method to IntroLab and relevant test in IntroLabTest

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -40,7 +40,9 @@ public class IntroLab {
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
-
+    public static String changs70() {
+        return printLabMessage("Hello", "Seongmin", true, "I like cats.");
+    }
     /**
      * An example method that calls on printLabMessage.
      */
@@ -52,7 +54,7 @@ public class IntroLab {
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
-
+        System.out.println(changs70());
         /* TODO: Add a call to the method you wrote in this main method,
          *        and print the results of it.
          *
--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -25,9 +25,16 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+    @Test(timeout = 1000)
+    public void testchangs70() {
+        String expected = "Hello! My name is Seongmin! " +
+                "I like cats more than dogs! I like cats.";
+        String actual = IntroLab.changs70();
+        assertEquals(expected, actual);
+        /* TODO: Write a test case for the method you wrote in IntroLab.
+         *       If done properly, you should be able to run IntroLabTest and see
+         *       the test results.
+         * As a reference, we've included testExampleStudent above.
+         */
+    }
 }
