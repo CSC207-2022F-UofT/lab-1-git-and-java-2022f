85731a83e725385e15a105c4ccfb4204a51c9806
VivianLiu2357 <vivianliu2357@gmail.com>
2022-09-12 19:35:17 -0400

    Committing...

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 1000)
+    public void testliuyut35() {
+        String expected = "Hi! My name is Vivian! " +
+                "I like cats more than dogs! I'm in CSC207!";
+        String actual = IntroLab.liuyut35();
+        assertEquals(expected, actual);
+    }
 }
