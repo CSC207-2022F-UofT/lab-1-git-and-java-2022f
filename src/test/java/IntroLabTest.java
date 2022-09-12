71d46d72db25d149a25d26a67a7b2eb1ef8805ad
shape-warrior-t <bl.swt1@gmail.com>
2022-09-12 19:31:43 -0400

    Completed TODOs

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -25,9 +25,9 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+    @Test(timeout = 10)
+    public void test_luanbo2(){
+        String expected = "Hello! My name is Bonan! I like dogs more than cats! Bark";
+        assertEquals(expected, IntroLab.luanbo2());
+    }
 }
