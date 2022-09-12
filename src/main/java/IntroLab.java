2a70ca6c18b9978d74f0c5a59eb69a6e40513b52
PhiliDips <philharker88@gmail.com>
2022-09-12 19:44:30 -0400

    Completed TODOs in IntroLab and IntroLabTest

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -33,13 +33,14 @@ public class IntroLab {
         return message_to_print + message;
     }
 
-    /* TODO: In the space below, write your our method.
-     *       The method should be named with your utorID and should call
-     *       printLabMessage with some parameters.
-     *
-     * As a reference, we've provided the sample method exampleStudent.
-     * Make sure you document your method properly!
+    /**
+     * Returns a message build around Philip Harker, making use of
+     * the printLabMessage() method.
      */
+    public static String harkerph() {
+        return printLabMessage("Salutations", "Philip Harker", true,
+                "Climate Action Now!");
+    }
 
     /**
      * An example method that calls on printLabMessage.
@@ -52,17 +53,9 @@ public class IntroLab {
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
-
-        /* TODO: Add a call to the method you wrote in this main method,
-         *        and print the results of it.
-         *
-         * As a reference, we've provided a sample call to exampleStudent
-         * above. Afterwards: run this file to see the output!
-         */
+        System.out.println(harkerph());
 
 
     }
-
-    // TODO: Go to test/java/IntroLabTest.java afterwards and complete the TODO.
 
 }
--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -25,9 +25,11 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+
+    public void testHarkerph() {
+        String expected = "Salutations! My name is Philip Harker!" +
+                "I like cats more than dogs! Climate Action Now!";
+        String actual = IntroLab.harkerph();
+        assertEquals(expected, actual);
+    }
 }
