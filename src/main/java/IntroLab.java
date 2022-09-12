ac4e133743ca8f1626d552af98da29cf48a403cf
woods <j2hashz0p@outlook.com>
2022-09-12 15:31:04 -0400

    Added Method Names to IntroLab and Made a Test in IntoLabTest

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -41,6 +41,13 @@ public class IntroLab {
      * Make sure you document your method properly!
      */
 
+    public static String rambaudj() {
+        return printLabMessage("Yo", "Janssen", false,
+                "message message message");
+    }
+
+
+
     /**
      * An example method that calls on printLabMessage.
      */
@@ -52,6 +59,8 @@ public class IntroLab {
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
+
+        System.out.println(rambaudj());
 
         /* TODO: Add a call to the method you wrote in this main method,
          *        and print the results of it.
--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,15 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+
+
+    @Test(timeout = 100)
+    public void testMyself() {
+        String expected = "Yo! My name is Janssen! " +
+                "I like dogs more than cats! message message message";
+
+        String actual = IntroLab.rambaudj();
+        assertEquals(expected, actual);
+    }
 }
