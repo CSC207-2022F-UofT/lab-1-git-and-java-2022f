0448140e7da4f87cd875b2d9344ba34cca7402fa
SpaceJesusTM <sugihikoruo@gmail.com>
2022-09-12 15:47:55 -0400

    Added [ruosamue() and relevant test] to IntroLab and relevant test in IntroLabTest. Changed timeout in test cases.

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,8 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+//    @Test(timeout = 100)
+@Test()
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +31,13 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+//    @Test(timeout = 100)
+    @Test()
+    public void testruosamue() {
+        String expected = "Hello! My name is Samuel! " +
+                "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.ruosamue();
+        assertEquals(expected, actual);
+    }
+
 }
