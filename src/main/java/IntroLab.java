7683f419539ded708d3ee3178e8261f3d4091abd
pengz <zhoujie.peng@mail.utoronto.ca>
2022-09-12 17:57:02 -0400

    Added Student() and testStudent() function

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -40,7 +40,10 @@ public class IntroLab {
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
-
+    public static String Student(){
+        return printLabMessage("Hello", "Collin", true,
+                "Welcome to CSC207!");
+    }
     /**
      * An example method that calls on printLabMessage.
      */
@@ -52,7 +55,7 @@ public class IntroLab {
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
-
+        System.out.println(Student());
         /* TODO: Add a call to the method you wrote in this main method,
          *        and print the results of it.
          *
