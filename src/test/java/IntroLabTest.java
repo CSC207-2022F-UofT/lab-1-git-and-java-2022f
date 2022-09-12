9b550b5fa389849be069ca5d935f2de0e298b66a
Diian-r <dianrong2012@gmail.com>
2022-09-12 17:44:29 -0400

    Did my thing.

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+    @Test(timeout = 100)
+    public void testrongDian() {
+        String expected = "Hi!!! My name is Dian! " + "I like cats more than dogs! The end.";
+        String actual = IntroLab.rongDian();
+        assertEquals(expected, actual);
+    }
 }
