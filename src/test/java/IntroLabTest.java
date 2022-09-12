c1da95ef155782d24a3a71a4f6128e5146ae8f07
a-llison-lau <allison.lau@mail.utoronto.ca>
2022-09-12 19:55:02 -0400

    Added laualli1 to IntroLab and relevant test in IntroLabTest.

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+    @Test
+    public void testlaualli1(){
+        String expected = "Hey! My name is Allison! " + "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.laualli1();
+        assertEquals(expected, actual);
+    }
 }
