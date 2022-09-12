d80381cb9213db2f3aef5b2b9fb8e25da0d10725
kiera <kieraclawsie@gmail.com>
2022-09-12 19:19:16 -0400

    Added method clawsiek to IntroLab and relevant test in IntroLabTest.

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -33,14 +33,17 @@ public class IntroLab {
         return message_to_print + message;
     }
 
-    /* TODO: In the space below, write your our method.
+    /*       In the space below, write your our method.
      *       The method should be named with your utorID and should call
      *       printLabMessage with some parameters.
      *
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
-
+    public static String clawsiek() {
+        return printLabMessage("Hello", "Kiera Clawsie", true, "Does anyone have a " +
+                "glass of water? I'm absolutely parched!");
+    }
     /**
      * An example method that calls on printLabMessage.
      */
