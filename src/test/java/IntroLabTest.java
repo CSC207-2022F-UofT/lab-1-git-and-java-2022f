bcaf277ecd759c83e3a8b83fb1250a18cbd31225
MaxLyu <113391218+Maxlyu254@users.noreply.github.com>
2022-09-12 20:09:24 -0400

    LABWORK
    completed the todos

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -25,9 +25,11 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
-    /* TODO: Write a test case for the method you wrote in IntroLab.
-     *       If done properly, you should be able to run IntroLabTest and see
-     *       the test results.
-     * As a reference, we've included testExampleStudent above.
-     */
+    @Test(timeout = 10)
+    public void testMaxLyu() {
+        String expected = "Hello! My name is Max Lyu! " +
+                "I like dogs more than cats! I don't want to go to school!";
+        String actual = IntroLab.maxLyu();
+        assertEquals(expected, actual);
+    }
 }
