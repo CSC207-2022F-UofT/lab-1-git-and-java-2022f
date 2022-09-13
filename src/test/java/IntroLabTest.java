375d9ee52a4b757c7aee7285234a91401dc500a8
tseamuscorlett <t.seamus.corlett@gmail.com>
2022-09-13 11:29:31 -0400

    testing lab1

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,13 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 100)
+    public void testCorlett3() {
+        String expected = "Hello! My name is Seamus Corlett! " +
+                "I like cats more than dogs! Here's my message!";
+        String actual = IntroLab.corlett3();
+        assertEquals(expected, actual);
+    }
 }
+
+
