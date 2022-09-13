ee96d3786e838ecdcae37a3be58c9d39c578e538
ericcui <cuihaoqian1120@gamil.com>
2022-09-12 20:46:27 -0400

    Adding methods and test case to IntroLab and IntroLabTest, fixing the estimated time error.

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -49,9 +49,15 @@ public class IntroLab {
                 "Welcome to CSC207!");
     }
 
+    public static String cuiHaoQi() {
+        return printLabMessage("Hello", "Eric Cui", true,
+                "Welcome to CSC207!");
+    }
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
+
+        System.out.println(cuiHaoQi());
 
         /* TODO: Add a call to the method you wrote in this main method,
          *        and print the results of it.
