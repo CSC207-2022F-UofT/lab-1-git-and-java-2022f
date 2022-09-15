09f7df40bdfce120487d21ffe04b18d3de7b8096
aryangoel24 <aryangoel24@gmail.com>
2022-09-15 19:14:44 -0400

    added methods

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -41,6 +41,11 @@ public class IntroLab {
      * Make sure you document your method properly!
      */
 
+    public static String goelary2() {
+        return printLabMessage("Hello there", "Aryan Goel", false,
+                "Welcome to CSC207!");
+    }
+
     /**
      * An example method that calls on printLabMessage.
      */
@@ -52,15 +57,7 @@ public class IntroLab {
 
     public static void main(final String[] args) {
         System.out.println(exampleStudent());
-
-        /* TODO: Add a call to the method you wrote in this main method,
-         *        and print the results of it.
-         *
-         * As a reference, we've provided a sample call to exampleStudent
-         * above. Afterwards: run this file to see the output!
-         */
-
-
+        System.out.println(goelary2());
     }
 
     // TODO: Go to test/java/IntroLabTest.java afterwards and complete the TODO.
